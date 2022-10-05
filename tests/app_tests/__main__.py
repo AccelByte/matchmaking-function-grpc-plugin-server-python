1dfe251f96296ec17131e1fe34a4a17d2cad46f6
Elmer Nocon <elmernocon@gmail.com>
2022-10-05 11:02:36 +0800

    test(app): add test module entrypoint

---
+++ tests/app_tests/__main__.py
@@ -0,0 +1,46 @@
+import unittest
+
+from argparse import ArgumentParser
+
+import app_tests
+
+
+def main(**kwargs):
+    loader = unittest.TestLoader()
+    runner = None
+
+    if (fmt := kwargs.get("format")) and fmt == "tap":
+        try:
+            import tap  # pip install tap.py
+
+            runner = tap.TAPTestRunner()
+            runner.set_stream(True)
+        except ImportError:
+            pass
+
+    if runner is None:
+        runner = unittest.TextTestRunner()
+
+    suite = loader.loadTestsFromModule(app_tests)
+    result = runner.run(suite)
+
+    if not result.wasSuccessful():
+        exit(1)
+
+
+def parse_args():
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "-f", "--format",
+        default="tap",
+        choices=("default", "tap"),
+    )
+
+    result = vars(parser.parse_args())
+
+    return result
+
+
+if __name__ == "__main__":
+    main(**parse_args())
