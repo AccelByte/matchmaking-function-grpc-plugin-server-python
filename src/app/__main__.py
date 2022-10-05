de7111a31e8709e8c0a318ff0a8db44d3a9443d7
Elmer Nocon <elmernocon@gmail.com>
2022-10-05 10:40:45 +0800

    feat(app): add module entrypoint

---
+++ src/app/__main__.py
@@ -0,0 +1,12 @@
+import json
+
+import app.proto.matchFunction_pb2 as match_func_proto
+import app.proto.matchFunction_pb2_grpc as match_func_grpc
+
+
+def main():
+    print("hello")
+
+
+if __name__ == "__main__":
+    main()
