c0611ec6e95a4ef54bd7fda58b32b45e571e0e6d
Elmer Nocon <elmernocon@gmail.com>
2022-10-12 09:26:17 +0800

    feat(app): update module entrypoint

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,42 +1,88 @@
-import concurrent.futures
-import logging
+import asyncio
 
+from argparse import ArgumentParser
+from logging import Logger
 from typing import Optional
 
-import grpc
+import grpc.aio
 
-import app.proto.matchFunction_pb2 as match_func_proto
-import app.proto.matchFunction_pb2_grpc as match_func_grpc
+from grpc_reflection.v1alpha import reflection
 
-import app.interceptor
 import app.logger
-import app.servicer
+
+import app.proto.matchFunction_pb2 as match_function_pb2
+import app.proto.matchFunction_pb2_grpc as match_function_grpc
+
+from app.interceptors.authorization import AuthorizationServerInterceptor
+from app.services.matchFunction import AsyncMatchFunctionService
 
 
-def main(max_workers: int = 10, logger: Optional[logging.Logger] = None, **kwargs) -> None:
+async def main(
+    *,
+    port: int,
+    enable_reflection: bool = False,
+    logger: Optional[Logger] = None,
+    **kwargs,
+) -> None:
     logger = logger if logger is not None else app.logger.DEFAULT_LOGGER
 
-    interceptors = (
-        app.interceptor.AuthorizationInterceptor(),
-        app.interceptor.DebugLoggerInterceptor(),
-        app.interceptor.ExceptionHandlingInterceptor(),
+    interceptors = []
+    interceptors.append(AuthorizationServerInterceptor())
+
+    server = grpc.aio.server(
+        interceptors=tuple(interceptors),
     )
-    server = grpc.server(
-        concurrent.futures.ThreadPoolExecutor(
-            max_workers=max_workers,
-        ),
-        interceptors=interceptors,
+    server.add_insecure_port(f"[::]:{port}")
+
+    service = AsyncMatchFunctionService(logger=logger)
+    match_function_grpc.add_MatchFunctionServicer_to_server(service, server)
+
+    if enable_reflection:
+        from grpc_reflection.v1alpha import reflection
+
+        SERVICE_NAMES = (
+            match_function_pb2.DESCRIPTOR.services_by_name["MatchFunction"].full_name,
+            reflection.SERVICE_NAME,
+        )
+        reflection.enable_server_reflection(SERVICE_NAMES, server)
+
+    await server.start()
+    await server.wait_for_termination()
+
+
+def parse_args():
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "-n",
+        "--host",
+        default="127.0.0.1",
+        type=str,
+        required=False,
+        help="Host[n]ame",
     )
 
-    servicer = app.servicer.MatchFunctionServicer()
-    match_func_grpc.add_MatchFunctionServicer_to_server(servicer, server)
+    parser.add_argument(
+        "-p",
+        "--port",
+        default=50051,
+        type=int,
+        required=False,
+        help="[P]ort",
+    )
 
-    server.add_insecure_port("[::]:50051")
-    server.start()
-    logger.info("server has started")
+    parser.add_argument(
+        "-r",
+        "--enable_reflection",
+        action="store_true",
+        required=False,
+        help="Enable Server [R]eflection",
+    )
 
-    server.wait_for_termination()
+    result = vars(parser.parse_args())
+
+    return result
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main(**parse_args()))
