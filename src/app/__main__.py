47462ad31fbaab97077dd66bfe5e80761eca0fe7
Elmer Nocon <elmernocon@gmail.com>
2022-11-17 16:16:01 +0800

    feat(app): add metrics interceptor

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -7,30 +7,24 @@ from logging import Logger
 from typing import Any, List, Optional, Union
 
 import grpc.aio
-import logging_loki
 
+from opentelemetry import metrics
 from opentelemetry import trace
-from opentelemetry.exporter.zipkin.proto.http import ZipkinExporter
 from opentelemetry.instrumentation.grpc import aio_server_interceptor
+from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-
-from accelbyte_py_sdk import AccelByteSDK
-from accelbyte_py_sdk.core import MyConfigRepository, InMemoryTokenRepository
 
 import app.logger
 
 import app.proto.matchFunction_pb2 as match_function_pb2
 import app.proto.matchFunction_pb2_grpc as match_function_grpc
 
-from app.auth.token_validator import TokenValidator
-from app.interceptors.authorization import AuthorizationServerInterceptor
-from app.interceptors.logging import DebugLoggingServerInterceptor
 from app.services.matchFunction import AsyncMatchFunctionService
 
 
-DEFAULT_APP_NAME: str = "app"
+DEFAULT_APP_NAME: str = "app-server"
 DEFAULT_APP_HOSTNAME: str = "localhost"
 DEFAULT_APP_PORT: int = 6565
 
@@ -42,6 +36,10 @@ DEFAULT_LOKI_URL: str = "http://localhost:3100/loki/api/v1/push"
 DEFAULT_LOKI_USERNAME: str = ""
 DEFAULT_LOKI_PASSWORD: str = ""
 
+DEFAULT_PROMETHEUS_ADDR: str = "0.0.0.0"
+DEFAULT_PROMETHEUS_PORT: int = 8080
+DEFAULT_PROMETHEUS_ENDPOINT: str = "/metrics"
+
 
 async def main(
     *,
@@ -52,33 +50,91 @@ async def main(
 ) -> None:
     logger = logger if logger is not None else app.logger.DEFAULT_LOGGER
 
+    enable_loki = arg2bool(os.environ.get("ENABLE_LOKI"), default=True)
+    enable_log2stderr = arg2bool(os.environ.get("ENABLE_LOG2STDERR"), default=False)
+    enable_prometheus = arg2bool(os.environ.get("ENABLE_PROMETHEUS"), default=True)
+    enable_zipkin = arg2bool(os.environ.get("ENABLE_ZIPKIN"), default=True)
+
+    enable_interceptor_auth = arg2bool(
+        os.environ.get("ENABLE_INTERCEPTOR_AUTH"), default=True
+    )
+    enable_interceptor_logging = arg2bool(
+        os.environ.get("ENABLE_INTERCEPTOR_LOGGING"), default=True
+    )
+    enable_interceptor_metrics = arg2bool(
+        os.environ.get("ENABLE_INTERCEPTOR_METRICS"), default=True
+    )
+
     # otel
     #   uses `APP_NAME`
     service_name = os.environ.get("APP_NAME", DEFAULT_APP_NAME)
     otel_resource = Resource(attributes={SERVICE_NAME: service_name})
-    otel_provider = TracerProvider(resource=otel_resource)
-    trace.set_tracer_provider(tracer_provider=otel_provider)
+    otel_metric_readers = []
+    otel_tracer_provider = TracerProvider(resource=otel_resource)
+    trace.set_tracer_provider(tracer_provider=otel_tracer_provider)
 
-    if arg2bool(os.environ.get("ENABLE_LOKI"), default=True):
+    if enable_loki:
         # loki
         #   uses `LOKI_URL`, `LOKI_USERNAME`, `LOKI_PASSWORD`
+        import logging_loki
+
         loki_url = os.environ.get("LOKI_URL", DEFAULT_LOKI_URL)
         loki_username = os.environ.get("LOKI_USERNAME", DEFAULT_LOKI_USERNAME)
         loki_password = os.environ.get("LOKI_PASSWORD", DEFAULT_LOKI_PASSWORD)
-        loki_auth = (loki_username, loki_password)
+        loki_auth = (loki_username, loki_password) if loki_username else None
         loki_handler = logging_loki.LokiHandler(
             url=loki_url, auth=loki_auth, version="1"
         )
         logger.addHandler(loki_handler)
         logger.info(f"loki enabled: {loki_url} ({loki_username}:****)")
 
-    if arg2bool(os.environ.get("ENABLE_LOG2STDERR"), default=False):
+    if enable_log2stderr:
         logger.addHandler(logging.StreamHandler())
         logger.info("stderr enabled")
 
-    if arg2bool(os.environ.get("ENABLE_ZIPKIN"), default=True):
+    if enable_prometheus:
+        # prometheus
+        #   uses 'PROMETHEUS_ADDR', 'PROMETHEUS_PORT', 'PROMETHEUS_ENDPOINT', 'PROMETHEUS_PREFIX'
+        import threading
+        from flask import Flask
+        from opentelemetry.exporter.prometheus import PrometheusMetricReader
+        from prometheus_client import start_http_server, make_wsgi_app
+        from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+        prometheus_addr = os.environ.get("PROMETHEUS_ADDR", DEFAULT_PROMETHEUS_ADDR)
+        prometheus_port = int(
+            arg2number(
+                os.environ.get("PROMETHEUS_PORT"), default=DEFAULT_PROMETHEUS_PORT
+            )
+        )
+        prometheus_endpoint = os.environ.get("PROMETHEUS_ENDPOINT", DEFAULT_PROMETHEUS_ENDPOINT)
+        prometheus_prefix = to_camelcase(
+            os.environ.get("PROMETHEUS_PREFIX", service_name)
+        )
+        flask_app = Flask(service_name)
+        flask_app.wsgi_app = DispatcherMiddleware(
+            app=flask_app.wsgi_app, mounts={prometheus_endpoint: make_wsgi_app()}
+        )
+
+        logger.info(
+            f"prometheus (flask) http server starting ({prometheus_addr}:{prometheus_port}{prometheus_endpoint})"
+        )
+        threading.Thread(
+            target=lambda: flask_app.run(
+                host=prometheus_addr,
+                port=prometheus_port,
+                debug=True,
+                use_reloader=False,
+            )
+        ).start()
+
+        otel_metric_readers.append(PrometheusMetricReader(prefix=prometheus_prefix))
+
+    if enable_zipkin:
         # zipkin
         #   uses `OTEL_EXPORTER_ZIPKIN_ENDPOINT`, `OTEL_EXPORTER_ZIPKIN_TIMEOUT`
+        from opentelemetry.exporter.zipkin.proto.http import ZipkinExporter
+
         zipkin_endpoint = os.environ.get("OTEL_EXPORTER_ZIPKIN_ENDPOINT")
         trace.get_tracer_provider().add_span_processor(
             span_processor=BatchSpanProcessor(
@@ -89,41 +145,52 @@ async def main(
         )
         logger.info(f"zipkin enabled: {zipkin_endpoint}")
 
-    # accelbyte
-    #   uses `APP_SECURITY_BASE_URL`, `APP_SECURITY_CLIENT_SECRET`, `APP_SECURITY_CLIENT_SECRET`, `APP_SECURITY_NAMESPACE`
-    ab_base_url = os.environ.get("APP_SECURITY_BASE_URL", DEFAULT_AB_BASE_URL)
-    ab_client_id = os.environ.get("APP_SECURITY_CLIENT_ID", None)
-    ab_client_secret = os.environ.get("APP_SECURITY_CLIENT_SECRET", None)
-    ab_namespace = get_env_var(
-        key=["APP_SECURITY_NAMESPACE", "NAMESPACE"], default="accelbyte"
-    )
-    ab_resource_name = os.environ.get(
-        "APP_SECURITY_RESOURCE_NAME", DEFAULT_AB_RESOURCE_NAME
-    )
-    accelbyte_sdk = AccelByteSDK()
-    accelbyte_sdk.initialize(
-        options={
-            "config": MyConfigRepository(
-                base_url=ab_base_url,
-                client_id=ab_client_id,
-                client_secret=ab_client_secret,
-                namespace=ab_namespace,
-            ),
-            "token": InMemoryTokenRepository(),
-        }
-    )
-    logger.info(
-        f"accelbyte initialized (base_url: {ab_base_url} client_id: {ab_client_id} namespace: {ab_namespace} resource_name: {ab_resource_name})"
-    )
+    if otel_metric_readers:
+        otel_meter_provider = MeterProvider(
+            resource=otel_resource, metric_readers=otel_metric_readers
+        )
+        metrics.set_meter_provider(meter_provider=otel_meter_provider)
+        if enable_prometheus:
+            logger.info(f"prometheus enabled")
 
     interceptors = [
         # opentelemetry aio server interceptor
         aio_server_interceptor(),
     ]
 
-    # authorization server interceptor
-    #   uses `APP_SECURITY_NAMESPACE`, `APP_SECURITY_RESOURCE_NAME`, `ENABLE_INTERCEPTOR_AUTH`, `TOKEN_VALIDATOR_FETCH_INTERVAL`
-    if arg2bool(os.environ.get("ENABLE_INTERCEPTOR_AUTH"), default=True):
+    if enable_interceptor_auth:
+        # authorization server interceptor
+        #   uses `APP_SECURITY_BASE_URL`, `APP_SECURITY_CLIENT_SECRET`, `APP_SECURITY_CLIENT_SECRET`, `APP_SECURITY_NAMESPACE`, `APP_SECURITY_RESOURCE_NAME`, `TOKEN_VALIDATOR_FETCH_INTERVAL`
+        from accelbyte_py_sdk import AccelByteSDK
+        from accelbyte_py_sdk.core import MyConfigRepository, InMemoryTokenRepository
+        from app.auth.token_validator import TokenValidator
+        from app.interceptors.authorization import AuthorizationServerInterceptor
+
+        ab_base_url = os.environ.get("APP_SECURITY_BASE_URL", DEFAULT_AB_BASE_URL)
+        ab_client_id = os.environ.get("APP_SECURITY_CLIENT_ID", None)
+        ab_client_secret = os.environ.get("APP_SECURITY_CLIENT_SECRET", None)
+        ab_namespace = get_env_var(
+            key=["APP_SECURITY_NAMESPACE", "NAMESPACE"], default="accelbyte"
+        )
+        ab_resource_name = os.environ.get(
+            "APP_SECURITY_RESOURCE_NAME", DEFAULT_AB_RESOURCE_NAME
+        )
+        accelbyte_sdk = AccelByteSDK()
+        accelbyte_sdk.initialize(
+            options={
+                "config": MyConfigRepository(
+                    base_url=ab_base_url,
+                    client_id=ab_client_id,
+                    client_secret=ab_client_secret,
+                    namespace=ab_namespace,
+                ),
+                "token": InMemoryTokenRepository(),
+            }
+        )
+        logger.info(
+            f"accelbyte initialized (base_url: {ab_base_url} client_id: {ab_client_id} namespace: {ab_namespace} resource_name: {ab_resource_name})"
+        )
+
         token_validator_fetch_interval = arg2number(
             os.environ.get("TOKEN_VALIDATOR_FETCH_INTERVAL"), default=3600
         )
@@ -145,9 +212,19 @@ async def main(
             )
         )
 
-    # debug logging server interceptor
-    #   uses `ENABLE_INTERCEPTOR_DEBUG`
-    if arg2bool(os.environ.get("ENABLE_INTERCEPTOR_DEBUG"), default=True):
+    if enable_interceptor_metrics:
+        import platform
+        from app.interceptors.metrics import MetricsServerInterceptor
+
+        metrics_labels = {
+            "os": platform.system().lower(),
+        }
+        interceptors.append(MetricsServerInterceptor(labels=metrics_labels))
+
+    if enable_interceptor_logging:
+        # debug logging server interceptor
+        from app.interceptors.logging import DebugLoggingServerInterceptor
+
         interceptors.append(DebugLoggingServerInterceptor(logger=logger))
 
     logger.info(
@@ -173,9 +250,9 @@ async def main(
         reflection.enable_server_reflection(service_names, server)
         logger.info("reflection enabled")
 
-    logger.info("server starting")
+    logger.info("grpc server starting")
     await server.start()
-    logger.info("server started")
+    logger.info("grpc server started")
     await server.wait_for_termination()
 
 
@@ -219,6 +296,10 @@ def get_env_var(key: Union[str, List[str]], default: Optional[str] = None) -> st
             if v is not None and v != "" and not v.isspace():
                 return v
         return default
+
+
+def to_camelcase(s: str) -> str:
+    return s.replace(" ", "_").replace("-", "_").replace(".", "_").strip()
 
 
 def parse_args():
